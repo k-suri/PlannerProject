97628cb7d4fd3a1ddfe656b6c05c9f0ff162cf06
lateef04 <122949139+lateef04@users.noreply.github.com>
2023-06-20 16:07:39 -0400

    displaying todo list in plannerSection

--- components/PlannerSection.js
+++ components/PlannerSection.js
@@ -1,25 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
-import { colors } from "../utils/Colors";
-const PlannerSection = ({ children, title }) => {
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/Colors';
+import { PlannerContext } from '../contexts/PlannerContext';
+
+const PlannerSection = ({ title }) => {
+  const plannerContext = useContext(PlannerContext);
+
   return (
-    <View style={style.section}>
-      <Text style={style.title}>{title}</Text>
-      {children}
+    <View style={styles.section}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        data={plannerContext.todos}
+        renderItem={({ item }) => (
+          <View style={styles.todoItem}>
+            <Text style={styles.todoText}>{item.text}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </View>
   );
 };
 
 export default PlannerSection;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   section: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 5,
     padding: 10,
     minHeight: 200,
     elevation: 4,
-    shadowColor: "#5A5A5A",
+    shadowColor: '#5A5A5A',
     shadowOffset: { height: 2, width: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 5,
@@ -29,7 +43,23 @@ const style = StyleSheet.create({
     borderBottomColor: colors.grayLight,
     borderBottomWidth: 3,
     paddingBottom: 2,
-    fontFamily: "Pacifico",
+    fontFamily: 'Pacifico',
     color: colors.secondary200,
+  },
+  todoItem: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  separator: {
+    height: 10,
   },
 });
